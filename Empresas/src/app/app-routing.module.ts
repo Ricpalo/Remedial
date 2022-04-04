0151e122f76cfd57aa81c940e56285fcbe39977c
Ricardo Daniel Palomares Arriaga <ricpalo001@gmail.com>
2022-04-04 05:23:44 -0500

    Arreglado

--- Empresas/src/app/app-routing.module.ts
+++ Empresas/src/app/app-routing.module.ts
@@ -22,65 +22,6 @@ const routes: Routes = [
     loadChildren: () => import('./form-usuario/form-usuario.module').then( m => m.FormUsuarioPageModule)
   },
   {
-    path: 'ubicaciones',
-    loadChildren: () => import('./ubicaciones/ubicaciones.module').then( m => m.UbicacionesPageModule)
-  },
-  {
-    path: 'instructores',
-    loadChildren: () => import('./instructores/instructores.module').then( m => m.InstructoresPageModule)
-  },
-  {
-    path: 'cursos',
-    loadChildren: () => import('./cursos/cursos.module').then( m => m.CursosPageModule)
-  },
-  {
-    path: 'alumnos',
-    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
-  },
-  {
-    path: 'eventos',
-    loadChildren: () => import('./eventos/eventos.module').then( m => m.EventosPageModule)
-  },
-  {
-    path: 'add-ubicacion',
-    loadChildren: () => import('./add-ubicacion/add-ubicacion.module').then( m => m.AddUbicacionPageModule)
-  },
-  {
-    path: 'update-ubicacion',
-    loadChildren: () => import('./update-ubicacion/update-ubicacion.module').then( m => m.UpdateUbicacionPageModule)
-  },
-  {
-    path: 'add-alumno',
-    loadChildren: () => import('./add-alumno/add-alumno.module').then( m => m.AddAlumnoPageModule)
-  },
-  {
-    path: 'update-alumno',
-    loadChildren: () => import('./update-alumno/update-alumno.module').then( m => m.UpdateAlumnoPageModule)
-  },
-  {
-    path: 'add-instructor',
-    loadChildren: () => import('./add-instructor/add-instructor.module').then( m => m.AddInstructorPageModule)
-  },
-  {
-    path: 'update-instructor',
-    loadChildren: () => import('./update-instructor/update-instructor.module').then( m => m.UpdateInstructorPageModule)
-  },
-  {
-    path: 'add-curso',
-    loadChildren: () => import('./add-curso/add-curso.module').then( m => m.AddCursoPageModule)
-  },
-  {
-    path: 'update-curso',
-    loadChildren: () => import('./update-curso/update-curso.module').then( m => m.UpdateCursoPageModule)
-  },
-  {
-    path: 'add-evento',
-    loadChildren: () => import('./add-evento/add-evento.module').then( m => m.AddEventoPageModule)
-  },
-  {
-    path: 'update-evento',
-    loadChildren: () => import('./update-evento/update-evento.module').then( m => m.UpdateEventoPageModule)
-  },  {
     path: 'empleados',
     loadChildren: () => import('./empleados/empleados.module').then( m => m.EmpleadosPageModule)
   },
